79209f44f501128a6d96894aab81835771efaf9e
siter_glb <1241077378@qq.com>
2021-02-17 19:29:09 +0800

    登录功能

--- hr-server/src/main/java/com/chen/server/service/IAdminService.java
+++ hr-server/src/main/java/com/chen/server/service/IAdminService.java
@@ -2,6 +2,9 @@ package com.chen.server.service;
 
 import com.chen.server.pojo.Admin;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.chen.server.pojo.RespBean;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * <p>
@@ -13,4 +16,19 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface IAdminService extends IService<Admin> {
 
+    /**
+     * 登录之后返回token
+     * @param username
+     * @param password
+     * @param request
+     * @return
+     */
+    RespBean login(String username, String password, HttpServletRequest request);
+
+    /**
+     *根据用户名获取用户
+     * @param username
+     * @return
+     */
+    Admin getAdminByUserName(String username);
 }
